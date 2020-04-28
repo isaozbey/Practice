074ae5d2b55bb0d434f4293a70700ce4cfeb27bb
isaozbey <64283159+isaozbey@users.noreply.github.com>
2020-04-28 15:50:16 -0400

    Update addCommand.java

--- src/tests/addCommand.java
+++ src/tests/addCommand.java
@@ -3,5 +3,11 @@ package tests;
 public class addCommand {
     public static void main(String[] args) {
         //I want to try git add file namecle
+        
+        System.out.println("Happy Birthday Java");
+        
+        
+        // Happy Birthday
+        
     }
 }
